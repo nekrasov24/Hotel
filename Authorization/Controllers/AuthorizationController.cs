2ff58c935d62f08f9027dc9c1c19ae98a93684cf
ICX\Dzmitry.Nekrasau <Dzmitry.Nekrasau@itechart-group.com>
2020-11-11 17:00:05 +0300

    first commit

---
+++ Authorization/Controllers/AuthorizationController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Authorization.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthorizationController : ControllerBase
+    {
+        [HttpGet("authorization")]
+        public string Message()
+        {
+            return "hello";
+        }
+    }
+}
