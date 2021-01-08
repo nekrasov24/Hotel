a7dca3709585c090114480bdcef3e5e047788b65
ICX\Dzmitry.Nekrasau <Dzmitry.Nekrasau@itechart-group.com>
2021-01-08 18:54:51 +0300

    Make LogService

---
+++ LogService/LogService/LogService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LogService.LogService
+{
+    public class LogService
+    {
+        private readonly ILogger _logger;
+
+        public LogService(ILogger<LogService> logger)
+        {
+            _logger = logger;
+        }
+
+
+    }
+
+}
--- LogService/Subscribe/Subscriber.cs
+++ LogService/Subscribe/Subscriber.cs
@@ -1,5 +1,6 @@
 ﻿using EasyNetQ;
 using EasyNetQ.Consumer;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +11,12 @@ namespace LogService.Subscribe
     public class Subscriber : ISubscriber
     {
         private readonly IAdvancedBus _bus;
+        private readonly ILogger _logger;
 
-        public Subscriber(IAdvancedBus bus)
+        public Subscriber(IAdvancedBus bus, ILogger<Subscriber> logger)
         {
             _bus = bus;
+            _logger = logger;
         }
 
         public void Subscribe()
@@ -23,7 +26,7 @@ namespace LogService.Subscribe
             {
                 registration.Add<PublishMessage>((message, info) => { Console.WriteLine("Body: {0}", message.Body); });
             });
-
+            _logger.LogInformation("Rammstein");
             //_bus.PubSub.Subscribe<PublishMessage>("my_subscription_id", msg => Console.WriteLine(msg.Text), new Action<ISubscriptionConfiguration>(o => o.WithTopic("asd")));
         }
     }     
